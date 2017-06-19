4053d231fa94af1175c65ce42441c743a867bd5e
Spencer Chan <schan1031@gmail.com>
2017-06-19 15:28:52 -0700

    added show, update, destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,12 +16,42 @@ class UsersController < ApplicationController
   end
 
   def show
-    render json: params
+    @user = selected_user
+
+    if @user
+      render json: @user
+    else
+      render json: ["Cannot find user!"], status: 404
+    end
+  end
+
+  def update
+    @user = selected_user
+
+    if @user.update_attributes(user_params)
+      render json: @user
+    else
+      render json: @user.error.full_messages, status: 400
+    end
+  end
+
+  def destroy
+    @user = selected_user
+
+    if @user
+      @user.destroy
+      render json: @user
+    else
+      render json: ["Cannot delete user!"], status: 404 
   end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :email)
+  end
+
+  def selected_user
+    User.find_by(id: params[:id])
   end
 end
